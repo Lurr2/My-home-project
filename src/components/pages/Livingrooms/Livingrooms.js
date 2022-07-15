50c9ebc066e104aad9bdcb72f5e34c61a6bb79be
Lurr2 <jkasperski1995@gmail.com>
2022-07-15 10:54:53 +0200

    error

--- src/components/pages/Livingrooms/Livingrooms.js
+++ src/components/pages/Livingrooms/Livingrooms.js
@@ -1,55 +1,75 @@
 import React, { useState } from 'react';
 import data from '../../../data.json';
 import Modal from '../../common/ModalLivingRoom/Modal';
+import './Livingrooms.scss';
 
 const Livingrooms = () => {
 
     const [clickedImg, setClickedImg] = useState(null);
     const [currentIndex, setCurrentIndex] = useState(null);
-
-
-
+  
     const handleClick = (item, index) => {
-        setCurrentIndex(index);
-        setClickedImg(item.image);
+      setCurrentIndex(index);
+      setClickedImg(item.link);
     };
-
-    const HandleRotationRight = () => {
-        const totalLength = data.LivingRooms.length;
-        if (currentIndex + 1 >= totalLength) {
-            setCurrentIndex(0);
-            const newUrl = data.LivingRooms[0].image;
-            setClickedImg(newUrl);
-            return;
-        }
-        const newIndex = currentIndex + 1;
-        const newUrl = data.LivingRooms.filter((item) => {
-            return data.LivingRooms.indexOf(item) === newIndex;
-        });
-        const newItem = newUrl[0].image;
-        setClickedImg(newItem);
-        setCurrentIndex(newIndex);
-    }
-
+  
+    const handelRotationRight = () => {
+      const totalLength = data.data.length;
+      if (currentIndex + 1 >= totalLength) {
+        setCurrentIndex(0);
+        const newUrl = data.data[0].link;
+        setClickedImg(newUrl);
+        return;
+      }
+      const newIndex = currentIndex + 1;
+      const newUrl = data.data.filter((item) => {
+        return data.data.indexOf(item) === newIndex;
+      });
+      const newItem = newUrl[0].link;
+      setClickedImg(newItem);
+      setCurrentIndex(newIndex);
+    };
+  
+    const handelRotationLeft = () => {
+      const totalLength = data.LivingRooms.length;
+      if (currentIndex === 0) {
+        setCurrentIndex(totalLength - 1);
+        const newUrl = data.LivingRooms[totalLength - 1].link;
+        setClickedImg(newUrl);
+        return;
+      }
+      const newIndex = currentIndex - 1;
+      const newUrl = data.LivingRooms.filter((item) => {
+        return data.LivingRooms.indexOf(item) === newIndex;
+      });
+      const newItem = newUrl[0].link;
+      setClickedImg(newItem);
+      setCurrentIndex(newIndex);
+    };
+  
     return (
-        <section>
-
-            {data.LivingRooms.map((item, index) => (
-
-                <div key={index}>
-                    <img src={item.image} alt="some" onClick={() => handleClick(item, index)} />
-                    <h2>{item.description}</h2>
-                </div>
-            ))}
-            {clickedImg && (
-                <Modal clickedImg={clickedImg}
-                    HandleRotationRight={HandleRotationRight}
-                    setClickedImg={setClickedImg}
-                />
-            )}
-        </section>
-
+      <div className="wrapper">
+        {data.data.map((item, index) => (
+          <div key={index} className="wrapper-images">
+            <img
+              src={item.link}
+              alt={item.text}
+              onClick={() => handleClick(item, index)}
+            />
+            <h2>{item.text}</h2>
+          </div>
+        ))}
+        <div>
+          {clickedImg && (
+            <Modal
+              clickedImg={clickedImg}
+              handelRotationRight={handelRotationRight}
+              setClickedImg={setClickedImg}
+              handelRotationLeft={handelRotationLeft}
+            />
+          )}
+        </div>
+      </div>
     );
-};
-
+  }
 export default Livingrooms;
